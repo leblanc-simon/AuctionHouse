16d3918be006d8976961b81b2ef4c04d0923422d
Simon Fry <simon@frys.me.uk>
2014-02-16 14:51:27 +0000

    Can enter password with return rather than clicking.

--- auction.js
+++ auction.js
@@ -37,7 +37,35 @@ if (Meteor.isClient) {
 	Session.setDefault('bidErrorMessage', "");
 	Session.setDefault('bidderName', "");
 
-	calculateAuctionTimeRemaining = function () {
+	// Returns an event map that handles the "escape" and "return" keys and
+  // "blur" events on a text input (given by selector) and interprets them
+  // as "ok" or "cancel".
+  var okCancelEvents = function (selector, callbacks) {
+    var ok = callbacks.ok || function () {};
+    var cancel = callbacks.cancel || function () {};
+
+    var events = {};
+    events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
+      function (evt) {
+        if (evt.type === "keydown" && evt.which === 27) {
+          // escape = cancel
+          cancel.call(this, evt);
+
+        } else if (evt.type === "keyup" && evt.which === 13 ||
+                   evt.type === "focusout") {
+          // blur/return/enter = ok/submit if non-empty
+          var value = String(evt.target.value || "");
+          if (value)
+            ok.call(this, value, evt);
+          else
+            cancel.call(this, evt);
+        }
+      };
+
+    return events;
+  };
+
+	var calculateAuctionTimeRemaining = function () {
 		if (AuctionDetails.findOne()) {
 			var auctionEndTime = moment(AuctionDetails.findOne().endDateTime);
 			if (moment().isAfter(auctionEndTime)) {
@@ -151,6 +179,15 @@ if (Meteor.isClient) {
 			}
 		}
 	});
+
+	Template.logs.events(okCancelEvents(
+		"#inputAdminPassword",
+		{
+			ok: function (value) {
+				Meteor.loginWithPassword("admin", value);
+			}
+		}
+	));
 
 	Template.logs.events({
 		'click #submitAdminPassword' : function (event, template) {
