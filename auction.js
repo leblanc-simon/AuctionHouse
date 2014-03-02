280ee7fd5fb7d33d802f0efd708bad60a9e24706
Simon Fry <simon@frys.me.uk>
2014-03-02 17:20:43 +0000

    Merge of changes from private branch for a specific auction, made generic, containing loads of improvements.

--- auction.js
+++ auction.js
@@ -17,13 +17,65 @@ Router.map(function () {
 		path: '/admin'
 	});
 
-	this.route('logs', {
-		path: '/logs'
-	});
-
 	this.route('bigScreen', {
 		path: '/bigscreen'
 	});
+});
+
+Meteor.methods({
+  makeBid: function (bidderName, newBid, item) {
+  	var previousBid = Bids.findOne({itemId: item._id}, {sort: {bid: -1}});
+  	var auctionEndTime = moment(AuctionDetails.findOne().endDateTime);
+
+  	var bidderNameValid = (bidderName != "" && bidderName != null);
+		var newBidValid = (newBid != null && newBid != NaN && newBid > 0);
+		var newBidBeatsOldBid = (previousBid == null || newBid > previousBid.bid);
+		var auctionIsOpen = moment().isBefore(auctionEndTime);
+
+		if (bidderNameValid && newBidValid && newBidBeatsOldBid && auctionIsOpen) {
+			return Bids.insert({
+				bidder: bidderName,
+				itemName: item.name,
+				itemId: item._id,
+				bid: newBid,
+				dateTime: new Date()
+			});
+	  }
+	},
+
+	deleteBid: function (bidId) {
+		if (!this.userId) {
+      throw new Meteor.Error(403, "You must be logged in");
+    }
+
+    return Bids.remove(bidId);
+	},
+
+	changeAuctionEndTime: function (newEndTime) {
+		if (!this.userId) {
+      throw new Meteor.Error(403, "You must be logged in");
+    }
+
+    console.log(newEndTime);
+
+    var detailsId = AuctionDetails.findOne()._id;
+		return AuctionDetails.update(
+			detailsId,
+			{$set: {endDateTime: newEndTime}});
+	},
+
+	upsertAuctionItems: function () {
+  	// Put in manual items
+  	for (var i = 1; i <= 12; i++) {
+  			Items.upsert({ order: i }, {
+				name: "Lot " + i,
+				description: "Some sort of description",
+				donatedBy: "Kindly donated by someone",
+				image: "http://lorempixel.com/276/155/abstract/" + i + "/",
+				order: i
+			});
+  	}
+  }
 });
 
 if (Meteor.isClient) {
@@ -33,10 +85,14 @@ if (Meteor.isClient) {
 
 	Session.setDefault('auctionHasEnded', false);
 	Session.setDefault('auctionEndTime', "");
+	Session.setDefault('auctionHoursRemaining', "00");
+	Session.setDefault('auctionMinutesRemaining', "00");
+	Session.setDefault('auctionSecondsRemaining', "00");
 	Session.setDefault('bidErrorItem', "");
 	Session.setDefault('bidErrorMessage', "");
 	Session.setDefault('bidderName', "");
 	Session.setDefault('bigScreenPage', 0);
+	Session.setDefault('clientTimeOffset', 0);
 
 	// Returns an event map that handles the "escape" and "return" keys and
   // "blur" events on a text input (given by selector) and interprets them
@@ -46,14 +102,13 @@ if (Meteor.isClient) {
     var cancel = callbacks.cancel || function () {};
 
     var events = {};
-    events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
+    events['keyup '+selector+', keydown '+selector] =
       function (evt) {
         if (evt.type === "keydown" && evt.which === 27) {
           // escape = cancel
           cancel.call(this, evt);
 
-        } else if (evt.type === "keyup" && evt.which === 13 ||
-                   evt.type === "focusout") {
+        } else if (evt.type === "keyup" && evt.which === 13) {
           // blur/return/enter = ok/submit if non-empty
           var value = String(evt.target.value || "");
           if (value)
@@ -69,14 +124,25 @@ if (Meteor.isClient) {
 	var calculateAuctionTimeRemaining = function () {
 		if (AuctionDetails.findOne()) {
 			var auctionEndTime = moment(AuctionDetails.findOne().endDateTime);
-			if (moment().isAfter(auctionEndTime)) {
+			var now = moment().subtract('ms', Session.get('clientTimeOffset'));
+			if (now.isAfter(auctionEndTime)) {
 				Session.set('auctionHasEnded', true);
 			} else {
-				var timeLeft = moment.duration(auctionEndTime.subtract(moment()));
+				Session.set('auctionHasEnded', false);
+				var timeLeft = moment.duration(auctionEndTime.subtract(now));
 				var timeRemainingString = timeLeft.hours() + " hours, " + timeLeft.minutes() + " minutes, " + timeLeft.seconds() + " seconds";
 				Session.set('auctionEndTime', timeRemainingString);
+				Session.set('auctionHoursRemaining', pad(timeLeft.hours(), 2));
+				Session.set('auctionMinutesRemaining', pad(timeLeft.minutes(), 2));
+				Session.set('auctionSecondsRemaining', pad(timeLeft.seconds(), 2));
 			}
 		}
+	}
+
+	function pad(num, size) {
+    var s = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
 	}
 
 	var cycleBigScreenItems = function () {
@@ -91,11 +157,32 @@ if (Meteor.isClient) {
 		}
 	}
 
+	var syncServerTime = function () {
+		Meteor.call('getServerTime', function (error, result) {
+			Session.set('clientTimeOffset', moment().diff(result));
+		});
+	}
+
+	var setDevice = function () {
+		Session.set('isBeingViewedOnMobile', $(window).width() < 768);
+	}
+
+	var truncateString = function (string, maxLength) {
+		if (string.length > maxLength) {
+			return string.substring(0,maxLength) + "...";
+		}
+
+		return string;
+	}
+
 	Meteor.setInterval(calculateAuctionTimeRemaining, 1000);
 	Meteor.setInterval(cycleBigScreenItems, 20000);
+	Meteor.setInterval(syncServerTime, 90000);
 
 	Meteor.startup(function () {
 		calculateAuctionTimeRemaining();
+		setDevice();
+		syncServerTime();
 	});
 
 	Template.item.bid = function () {
@@ -118,13 +205,21 @@ if (Meteor.isClient) {
 		return Session.get('bidErrorMessage');
 	};
 
+	Template.item.itemRow = function () {
+		return Math.floor((this.order - 1) / 3);
+	};
+
+	Template.item.preserve({
+		'input[id]': function (node) { return node.id; }
+	});
+
 	Template.main.items = function () {
 		return Items.find();
 	};
 
 	Template.main.showAuctionItems = function () {
 		return Session.get('bidderName') != "";
-	}
+	};
 
 	Template.countdown.auctionTimeRemaining = function () {
 		return Session.get('auctionEndTime');
@@ -134,11 +229,28 @@ if (Meteor.isClient) {
 		return Session.get('auctionHasEnded');
 	};
 
-	Template.bigScreen.items = function () {
-		var numberToSkip = Session.get('bigScreenPage') * 8;
+	Template.countdown.auctionHoursRemaining = function () {
+		return Session.get('auctionHoursRemaining');
+	};
+
+	Template.countdown.auctionMinutesRemaining = function () {
+		return Session.get('auctionMinutesRemaining');
+	};
+
+	Template.countdown.auctionSecondsRemaining = function () {
+		return Session.get('auctionSecondsRemaining');
+	};
+
+	Template.bigScreen.itemsPartOne = function () {
 		return Items.find({}, {
-			skip: numberToSkip,
-			limit: 8
+			limit: 5
+		});
+	};
+
+	Template.bigScreen.itemsPartTwo = function () {
+		return Items.find({}, {
+			skip: 5,
+			limit: 11
 		});
 	};
 
@@ -148,9 +260,9 @@ if (Meteor.isClient) {
 		}
 	};
 
-	Template.bigScreenItem.highestBidder = function () {
+	Template.bigScreenItem.highestBidderTruncated = function () {
 		if (Bids.findOne({itemId: this._id}, {sort: {bid: -1}})) {
-			return Bids.findOne({itemId: this._id}, {sort: {bid: -1}}).bidder;
+			return truncateString(Bids.findOne({itemId: this._id}, {sort: {bid: -1}}).bidder, 15);
 		}
 	};
 
@@ -158,7 +270,7 @@ if (Meteor.isClient) {
 		$('.datetimepicker').datetimepicker();
 	};
 
-	Template.logs.logs = function () {
+	Template.admin.logs = function () {
 		return Bids.find({}, {sort: {dateTime: -1}});
 	};
 
@@ -167,10 +279,21 @@ if (Meteor.isClient) {
 	};
 
 	Template.main.events({
-		'keyup #bidderName' : function (event, template) {
+		'keypress #bidderName' : function (event, template) {
+			Session.set('bidderName', template.find('#bidderName').value);
+		},
+		'blur #bidderName' : function (event, template) {
 			Session.set('bidderName', template.find('#bidderName').value);
 		}
 	});
+
+	Template.main.events(okCancelEvents(
+		"#bidderName",
+		{
+			ok: function (value) {
+				Session.set('bidderName', value);
+			}
+		}));
 
 	Template.item.events({
 		'click #submitBid' : function (event, template) {
@@ -179,16 +302,20 @@ if (Meteor.isClient) {
 			var item = this;
 			var previousBid = Bids.findOne({itemId: item._id}, {sort: {bid: -1}});
 
-			if ((bidderName != "" && bidderName != null) && (previousBid == null || newBid > previousBid.bid) && !Session.get('auctionHasEnded')) {
-				Bids.insert({
-					bidder: bidderName,
-					itemName: item.name,
-					itemId: item._id,
-					bid: newBid,
-					dateTime: new Date()
-				});
+			var bidderNameValid = (bidderName != "" && bidderName != null);
+			var newBidValid = (newBid != null && newBid != NaN && newBid > 0);
+			var newBidBeatsOldBid = (previousBid == null || newBid > previousBid.bid);
+			var auctionIsOpen = !Session.get('auctionHasEnded');
+
+			if (bidderNameValid && newBidValid && newBidBeatsOldBid && auctionIsOpen) {
+				Meteor.call(
+					'makeBid',
+					bidderName,
+					newBid,
+					item);
 				Session.set('bidErrorItem', "");
 				Session.set('bidErrorMessage', "");
+				template.find('.newBid').value = "";
 			} else {
 				Session.set('bidErrorItem', item._id);
 				if (bidderName == "" || bidderName == null) {
@@ -199,20 +326,66 @@ if (Meteor.isClient) {
 					Session.set('bidErrorMessage', "The auction has ended. You can no longer bid on items.");
 				}
 			}
+		},
+		'click .dismissError' : function (event, template) {
+			Session.set('bidErrorItem', "");
+			Session.set('bidErrorMessage', "");
 		}
 	});
 
-	Template.admin.events({
+	Template.item.events(okCancelEvents(
+		"#newBid",
+		{
+			ok: function (value, template) {
+				var bidderName = Session.get('bidderName');
+				var newBid = parseInt(value, 10);
+				var item = this;
+				var previousBid = Bids.findOne({itemId: item._id}, {sort: {bid: -1}});
+
+				var bidderNameValid = (bidderName != "" && bidderName != null);
+				var newBidValid = (newBid != null && newBid != NaN && newBid > 0);
+				var newBidBeatsOldBid = (previousBid == null || newBid > previousBid.bid);
+				var auctionIsOpen = !Session.get('auctionHasEnded');
+
+				if (bidderNameValid && newBidValid && newBidBeatsOldBid && auctionIsOpen) {
+					Meteor.call(
+						'makeBid',
+						bidderName,
+						newBid,
+						item);
+					Session.set('bidErrorItem', "");
+					Session.set('bidErrorMessage', "");
+					console.log(template);
+					template.target.value = "";
+				} else {
+					Session.set('bidErrorItem', item._id);
+					if (bidderName == "" || bidderName == null) {
+						Session.set('bidErrorMessage', "Please set your name at the top of the page.");
+					} else if (newBid <= previousBid.bid) {
+						Session.set('bidErrorMessage', "Your bid is not higher than the current highest bid. Please put in a higher bid. Bids are rounded down to the nearest pound.");
+					} else if (Session.get('auctionHasEnded')) {
+						Session.set('bidErrorMessage', "The auction has ended. You can no longer bid on items.");
+					}
+				}
+			},
+			cancel: function () {
+				template.find('#newBid').value = "";
+			}
+		}
+	));
+
+	Template.changeEndTime.events({
 		'click #submitAdminChanges' : function (event, template) {
-			var newAuctionEndDate = moment(template.find('#auctionDatePicker').value, 'DD/MM/YYYY h:mm a').toDate();
+			var newAuctionEndDate = moment(template.find('#auctionDatePicker').value, 'MM/DD/YYYY h:mm a').toDate();
 			if (newAuctionEndDate) {
-				var detailsId = AuctionDetails.findOne()._id;
-				AuctionDetails.update(detailsId, {$set: {endDateTime: newAuctionEndDate}});
+				Meteor.call(
+					'changeAuctionEndTime',
+					newAuctionEndDate);
 			}
 		}
 	})
 
-	Template.logs.events(okCancelEvents(
+	Template.admin.events(okCancelEvents(
 		"#inputAdminPassword",
 		{
 			ok: function (value) {
@@ -221,9 +394,13 @@ if (Meteor.isClient) {
 		}
 	));
 
-	Template.logs.events({
+	Template.admin.events({
 		'click #submitAdminPassword' : function (event, template) {
 			Meteor.loginWithPassword("admin", template.find('.adminPassword').value);
+		},
+
+		'click #reloadItemData' : function () {
+			Meteor.call('upsertAuctionItems');
 		},
 
 		'click #logOut' : function () {
@@ -234,7 +411,9 @@ if (Meteor.isClient) {
 	Template.logRow.events({
 		'click #deleteBid' : function (event, template) {
 			var bid = this;
-			Bids.remove(bid._id);
+			Meteor.call(
+				'deleteBid',
+				bid._id);
 		}
 	});
 }
@@ -254,14 +433,7 @@ if (Meteor.isServer) {
 
 	Meteor.startup(function () {
 		if (Items.find().count() === 0) {
-			for (var i = 1; i <= 12; i++) {
-				Items.insert({
-					name: "Item " + i,
-					description: "Some text",
-					image: "http://placehold.it/276x155/ff0000/ffffff",
-					order: i
-				});
-			}
+			Meteor.call('upsertAuctionItems');
 		}
 
 		if (AuctionDetails.find().count() === 0) {
@@ -277,4 +449,10 @@ if (Meteor.isServer) {
 			});
 		}
 	});
+
+  Meteor.methods({
+    getServerTime: function () {
+      return moment().toDate();
+    }
+  });
 }
