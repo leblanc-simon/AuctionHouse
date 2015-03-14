7bfdaa9f704053c8df1ad627c82ca4c67481ab2f
Simon Fry <simon@frys.me.uk>
2015-03-14 13:35:31 +0000

    Fixed order on big screen mode.

--- client/bigScreen/bigScreen.js
+++ client/bigScreen/bigScreen.js
@@ -1,13 +1,15 @@
 Template.bigScreen.helpers({
   itemsPartOne: function () {
     return Items.find({}, {
-      limit: 3
+      limit: 3,
+      sort: {order: 1}
     });
   },
   itemsPartTwo: function () {
     return Items.find({}, {
       skip: 3,
-      limit: 3
+      limit: 3,
+      sort: {order: 1}
     });
   },
   title: function () {
